c96549be6d776f4895191735306f099c626470b2
Tashby <tashby2714@gmail.com>
2020-08-23 18:12:57 -0400

    mvp

---
+++ api/Apiserver.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const db = require("../data/dbConfig.js");
+
+const router = express.Router()
+
+//GET
+router.get("/", async (req, res, next) => {
+    try {
+        const accounts = await db.select("*").from("accounts")
+        res.json(accounts)
+    } catch (err) {
+        next(err)
+    }
+})
+
+//GET
+router.get("/:id", async (req, res, next) => {
+    try {
+        const accounts = await db.select("*")
+            .from("accounts")
+            .where("id", req.params.id)
+            .limit(1)
+        res.json(accounts)
+    } catch (err) {
+        next(err)
+    }
+})
+
+//POST
+router.post("/", async (req, res, next) => {
+    try {
+        const [id] = await db
+            .insert({
+                name: req.body.name,
+                budget: req.body.budget,
+            })
+            .into("accounts")
+
+        const account = await db("accounts")
+            .where("id", id)
+            .first()
+
+        res.status(201).json(account)
+    } catch (err) {
+        next(err)
+    }
+})
+
+//PUT
+router.put("/:id", async (req, res, next) => {
+    try {
+        await db("accounts")
+            .update({
+                name: req.body.name,
+                budget: req.body.budget,
+            })
+            .where("id", req.params.id)
+
+        const account = await db("accounts")
+            .where("id", req.params.id)
+            .first()
+        res.json(account)
+    } catch (err) {
+        next(err)
+    }
+})
+
+//DELETE
+router.delete("/:id", async (req, res, next) => {
+    try {
+        await db("accounts")
+            .where("id", req.params.id)
+            .del()
+        res.status(204).end()
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const server = require("./api/server.js");
 
+const express = require("express");
+const Apiserver = require("./api/Apiserver.js");
+
+Apiserver.use(express.json());
 const PORT = process.env.PORT || 5000;
+
+
+const server = express();
+server.use(Apiserver)
+
+server.use((err, req, res, next)=>{
+  console.log(err)
+  res.status(500).json({
+    message: "Something went wrong"
+  })
+})
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
